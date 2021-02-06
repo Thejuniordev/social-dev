4ca25a862187e7e63c9ec220bd574796484835de
Junior Alves <developersmais@gmail.com>
2021-02-05 22:04:09 -0200

    Initial project

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,20 @@
+ import React from 'react';
+
+ function Header() {
+    return (
+        <header className="header">
+            <div className="toolbar">
+                <div className="">
+                    <span>Social DEV</span>
+                </div>
+                <div className="">
+                    <button>Novo Post</button>
+                    <span>IMg1</span>
+                    <span>IMg2 </span>
+                </div>
+            </div>
+        </header>
+    )
+ }
+
+ export default Header;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './components/Header';
+import './style.css';
+
+function Home() {
+    return (
+        <div>
+            <Header />
+
+            <main className="main">
+                <div className="nav">
+
+                </div>
+                <div className="feed">
+
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default Home;
